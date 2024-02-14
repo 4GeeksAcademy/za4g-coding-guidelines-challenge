a7df905712800011c1855ac471945b8358409574
za4g <138465437+za4g@users.noreply.github.com>
2024-02-14 18:09:05 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,21 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+	
+  let domain = [];
+  
+  for (let index in pronoun) {
+		domain[0] = pronoun[index];
+    for (let index in adj) {
+    	domain[1] = adj[index];
+      for (let index in noun) {
+      	domain[2] = noun[index];
+        console.log(domain[0] + domain[1] + domain[2] + ".com")
+      }
+    }
+  }   
 };
