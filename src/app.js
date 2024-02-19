295f599005590c6b5c2741fcce13e0ce7a3c746f
za4g <138465437+za4g@users.noreply.github.com>
2024-02-19 19:32:18 +0000

    Updated with Coding Standards

--- src/app.js
+++ src/app.js
@@ -9,20 +9,20 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-	
+  let pronounList = ["the", "our"];
+  let adjectiveList = ["great", "big"];
+  let nounList = ["jogger", "racoon"];
+
   let domain = [];
-  
-  for (let index in pronoun) {
-		domain[0] = pronoun[index];
-    for (let index in adj) {
-    	domain[1] = adj[index];
-      for (let index in noun) {
-      	domain[2] = noun[index];
-        console.log(domain[0] + domain[1] + domain[2] + ".com")
+
+  for (let pronoun in pronounList) {
+    domain[0] = pronounList[pronoun];
+    for (let adjective in adjectiveList) {
+      domain[1] = adjectiveList[adjective];
+      for (let noun in nounList) {
+        domain[2] = nounList[noun];
+        console.log(domain[0] + domain[1] + domain[2] + ".com");
       }
     }
-  }   
+  }
 };
