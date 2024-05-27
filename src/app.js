4e1961097b516f165221ec9718c3719f072d3398
za4g <138465437+za4g@users.noreply.github.com>
2024-05-27 16:58:35 +0000

    Coding Guidelines - Refactored

--- src/app.js
+++ src/app.js
@@ -9,20 +9,16 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
-  let pronounList = ["the", "our"];
-  let adjectiveList = ["great", "big"];
-  let nounList = ["jogger", "racoon"];
-
-  let domain = [];
-
-  for (let pronoun in pronounList) {
-    domain[0] = pronounList[pronoun];
-    for (let adjective in adjectiveList) {
-      domain[1] = adjectiveList[adjective];
-      for (let noun in nounList) {
-        domain[2] = nounList[noun];
-        console.log(domain[0] + domain[1] + domain[2] + ".com");
-      }
-    }
-  }
+  const pronouns = ["the", "our"];
+  const nouns = ["jogger", "racoon"];
+  const adjectives = ["great", "big"];
+  
+  pronouns.forEach(pronoun => {
+    adjectives.forEach(adjective => {
+      nouns.forEach(noun => {
+        console.log(`${pronoun}${adjective}${noun}.com`);
+      });
+    });
+  });
+  
 };
